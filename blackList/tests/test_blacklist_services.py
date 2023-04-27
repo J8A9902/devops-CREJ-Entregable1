601504f7d70f0eea75c016c1fb616504a8327e05
J8A9902 <j.ochoao@uniandes.edu.co>
2023-04-26 20:23:35 -0500

    Se completan pruebas de creación blackList

--- blackList/tests/test_blacklist_services.py
+++ blackList/tests/test_blacklist_services.py
@@ -3,11 +3,49 @@ import unittest
 from services import blackList_service
 
 class blackListTestCase(unittest.TestCase):
+    # Prueba de crear un blackList sin correo
+    def test_email_create_blackList(self):
+
+        test_response = blackList_service.create_blackList_service({'email':" ",
+                                                                    'app_uuid':"123",
+                                                                    'blocked_reason':"Mineria",
+                                                                    'ip':'123'})
+        print('----------------------------------------------')
+        print(test_response)
+
+        assert test_response[1] == 400
+    
+    # Prueba de crear un blackList sin APP_UUID
+    def test_uuid_create_blackList(self):
+
+        test_response = blackList_service.create_blackList_service({'email':"ejemplo@fdsfds.co",
+                                                                    'app_uuid':" ",
+                                                                    'blocked_reason':"Mineria",
+                                                                    'ip':'123'})
+        print('----------------------------------------------')
+        print(test_response)
+
+        assert test_response[1] == 400
+    
+    # Prueba de crear un blackList sin blocked reason
+    def test_reason_create_blackList(self):
+
+        test_response = blackList_service.create_blackList_service({'email':"ejemplo@fdsfds.co",
+                                                                    'app_uuid':"123",
+                                                                    'blocked_reason':" ",
+                                                                    'ip':'123'})
+        print('----------------------------------------------')
+        print(test_response)
+
+        assert test_response[1] == 400
+    
+    # Prueba de crear un blackList sin respuesta de la base de datos
     def test_create_blackList(self):
 
         test_response = blackList_service.create_blackList_service({'email':"ejemplo@fdsfds.co",
                                                                     'app_uuid':"123",
-                                                                    'blocked_reason':""})
+                                                                    'blocked_reason':"Mineria",
+                                                                    'ip':'123'})
         print('----------------------------------------------')
         print(test_response)
 
